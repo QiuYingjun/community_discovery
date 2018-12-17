380af45a2bc571a6eb6112782ab52a15d6b6d56d
Jimmy Zhou <moreconsci@gmail.com>
2018-12-17 16:54:48 +0800

    fix bug in models.py.

--- community/models.py
+++ community/models.py
@@ -29,7 +29,7 @@ class Community(models.Model):
     ip_counts = models.IntegerField(default=0)
     link_counts = models.IntegerField(default=0)
     purity = models.FloatField(default=0)
-    apps = models.CharField(max_length=512)
+    apps = models.CharField(max_length=512, default='')
 
     def __str__(self):
         return '{},{},{},{}'.format(self.community_tag, self.leader_ip, self.ip_counts, self.link_counts)
